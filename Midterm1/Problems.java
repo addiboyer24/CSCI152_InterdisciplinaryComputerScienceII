6ff77532bb2e567afd65aea77a01cf36269a4ab5
Addison Boyer <AddisonB@consumerdirectcare.com>
2023-09-27 11:16:53 -0600

    Removing more files

--- Midterm1/Problems.java
+++
@@ -1,9 +0,0 @@
-package Midterm1;
-
-public class Problems {
-    public static void main(String[] args) {
-        int i = 3;
-        System.out.println(i + 3 + "3");
-        // Answer string, "63"
-    }
-}
