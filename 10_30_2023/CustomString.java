aa7aebc5e7b71f3cbfdc463fc37bad3499cf0fd8
Addison Boyer <AddisonB@consumerdirectcare.com>
2023-10-30 14:56:13 -0600

    Adding custom string class

---
+++ 10_30_2023/CustomString.java
@@ -0,0 +1,42 @@
+public class CustomString{
+
+    // instance variables (declare here).
+    private char[] value;
+
+    private int length;
+
+    public CustomString(){ // Default constructor.
+    }
+
+    public CustomString(char[] value){ // custom constructor.
+        this.value = value;
+    }
+
+    public CustomString(String value){
+        this.value = new char[value.length()];
+        for (int i = 0; i < this.value.length; i++){
+            this.value[i] = value.charAt(i);
+        }
+    }
+
+    public int length(){
+        return this.value.length;
+    }
+
+    public char randomChar(){
+        int randomIndex = this.randomNumber(); // calling a private method within our class.
+        return this.value[randomIndex];
+    }
+
+    public char charAt(int i){
+        return this.value[i];
+    }
+
+    private int randomNumber(){
+        return (int)(Math.random() * this.value.length);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 10_30_2023/StringClient.java
@@ -0,0 +1,11 @@
+public class StringClient{
+    public static void main(String[] args) {
+        char[] string = {'h', 'i' };
+        CustomString myStringDefault = new CustomString(); // calling default constructor.
+        CustomString myString = new CustomString(string);
+        CustomString myString2 = new CustomString("hi");
+        int stringLength = myString2.length();
+        StdOut.println(stringLength);
+        StdOut.println(myString.length());
+    }
+}
