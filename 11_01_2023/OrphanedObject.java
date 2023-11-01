9d5fe5cda26109e3714cd9f815ef14b080903d79
Addison Boyer <AddisonB@consumerdirectcare.com>
2023-11-01 17:53:34 -0600

    blah

--- 11_01_2023/OrphanedObject.java
+++ 11_01_2023/OrphanedObject.java
@@ -1,7 +1,7 @@
 public class OrphanedObject{
     public static void main(String[] args) {
         String myString = new String("bye");
-        String orphanedString = "hello";
+        String orphanedString = "hello"; // "hello" is Orphaned :(
 
         orphanedString = myString;
 
