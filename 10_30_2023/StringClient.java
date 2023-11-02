9cbf6d9f9a6431fc38061c1994b4df28478c0769
Addison Boyer <AddisonB@consumerdirectcare.com>
2023-11-01 18:18:38 -0600

    adding class files.

--- 10_30_2023/StringClient.java
+++ 10_30_2023/StringClient.java
@@ -1,12 +1,20 @@
 
 public class StringClient{
     public static void main(String[] args) {
-        char[] string = {'h', 'i' };
+        /*char[] string = {'h', 'i' };
         CustomString myStringDefault = new CustomString(); // calling default constructor.
-        CustomString myString = new CustomString(string);
-        CustomString myString2 = new CustomString("hi");
-        int stringLength = myString2.length();
-        StdOut.println(stringLength);
-        StdOut.println(myString.length());
+        CustomString myString = new CustomString(string);*/
+        CustomString myStringFromString = new CustomString("hi");
+        CustomString sameString = myStringFromString.substring(0, 2);
+        CustomString shorterString = myStringFromString.substring(0, 1);
+
+        StdOut.printf("Before: %d , After: %d", myStringFromString.length(), sameString.length());
+        StdOut.printf("Before: %d , After: %d", myStringFromString.length(), shorterString.length());
+
+        // modifying object being referenced.
+        /*System.out.println(myString.charAt(0));
+        string[0] = 'i';
+        string[1] = 'h';
+        System.out.println(myString.charAt(0));*/
     }
 }
