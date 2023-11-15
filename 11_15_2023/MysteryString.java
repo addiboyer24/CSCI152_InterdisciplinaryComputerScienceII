cddf656156284c2eaa2d46f2be06bf437827f14c
Addison Boyer <AddisonB@consumerdirectcare.com>
2023-11-15 10:59:39 -0700

    Adding final jeapordy game

---
+++ 11_15_2023/MysteryString.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+import java.io.File;
+public class MysteryString {
+    public static String mysteryFunction(String inputString){
+        char[] mysteryString = new char[inputString.length()];
+        int lastIndex = mysteryString.length -1;
+        for (int i = lastIndex; i >= 0; i--){
+            mysteryString[lastIndex - i] = inputString.charAt(i);
+        }
+        return new String(mysteryString);
+    }
+
+    public static boolean isThisSpecialWord(String candidate){
+        return candidate.equals(mysteryFunction(candidate));
+    }
+
+    public static void printPrompt(String candidate){
+        StdOut.printf("%s \"%s\" this special word.\n\n", candidate, isThisSpecialWord(candidate) ? "is" : "is not");
+    }
+    public static void main(String[] args) throws IOException {
+        Scanner myScanner = new Scanner(new File("./mysterywords.txt"), Charset.defaultCharset());
+        String[] candidates = myScanner.next().split(",");
+        for (String candidate : candidates){
+            printPrompt(candidate);
+        }
+    }
+}
